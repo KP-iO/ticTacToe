24f33cb666447bc58086118af10825f543016714
Khrishawn <khrishawnpowell@gmail.com>
2019-11-29 15:32:12 +0000

    add first test - has a board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class Board {
+    public String getBoard() {
+        return "This is board.";
+    }
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BoardTest {
-
+    @Test
+    public void testHasABoard() {
+        Board board = new Board();
+        assertEquals("This is board.", board.getBoard());
+    }
 }
