bdbe605c270053096ed48f90609f17fc53dab99f
Khrishawn <khrishawnpowell@gmail.com>
2019-12-06 16:01:01 +0000

    ability to use constructor to check

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -3,27 +3,33 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Board {
+    private String playerOne;
+    private String playerTwo;
     private List<String> grid = new ArrayList<>(Arrays.asList("1","2","3","4","5","6","7","8","9"));
 
+    public Board(String playerOne, String playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
+
     public boolean isEmpty() {
-        return grid.contains("X") && grid.contains("0");
+        return grid.contains(playerOne) && grid.contains(playerTwo);
     }
 
     public List getBoard() {
-
         return grid;
     }
 
     public void markBoard(String mark, int position) {
         grid.set(position - 1, mark);
-
     }
 
     public String getMarkAtPosition(int position) {
         return grid.get(position - 1);
     }
 
-
-
-
+    public boolean isValidPosition(int position) {
+        String mark = getMarkAtPosition(position);
+        return mark != playerOne && mark != playerTwo;
+    }
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,27 +1,44 @@
 import org.junit.Test;
+import org.junit.Before;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class BoardTest {
+    private String playerOneMark = "X";
+    private String playerTwoMark = "O";
+    private Board board;
+
+    @Before
+    public void setUp() {
+        board = new Board(playerOneMark, playerTwoMark);
+    }
+
     @Test
     public void hasABoard() {
-        Board board = new Board();
         List currentGrid = board.getBoard();
+
         assertEquals(9, currentGrid.size());
     }
 
     @Test
     public void boardIsEmpty() {
-        Board board = new Board();
         assertEquals(false, board.isEmpty());
     }
 
     @Test
     public void marksTheBoard() {
-        Board board = new Board();
-        board.markBoard("X", 1);
-        assertEquals("X", board.getMarkAtPosition(1));
+        board.markBoard(playerOneMark, 1);
+
+        assertEquals(playerOneMark, board.getMarkAtPosition(1));
+    }
+
+    @Test
+    public void knowsIfAPositionIsValid() {
+        board.markBoard(playerOneMark, 1);
+        System.out.println(board.getBoard());
+
+        assertFalse(board.isValidPosition(1));
     }
 }
